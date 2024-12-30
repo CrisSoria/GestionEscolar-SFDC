c18f040c827723570b7e5409b04e91400eff6106
Cristian Soria <crissoria07@gmail.com>
2024-12-30 00:00:24 -0300

    Mejoras CSS y visuales

--- force-app/main/default/lwc/attendance/attendance.js
+++ force-app/main/default/lwc/attendance/attendance.js
@@ -28,7 +28,7 @@ export default class Attendance extends LightningElement {
     }
 
     get fechaActualFormateada() {
-        const diasSemana = ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado'];
+        const diasSemana = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
         const meses = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'];
     
         const fechaActual = new Date();
